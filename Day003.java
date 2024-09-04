cd3d410de6ade870809c755bec32d3920bd67f79
Maryamm05 <myam5040@gmail.com>
2024-09-04 11:03:04 +0800

    class3

---
+++ Day003.java
@@ -0,0 +1,10 @@
+public class Day003{
+  public static void main(String[] args) {
+      
+    System.out.println("Saya Maryam");
+    System.out.println("Alamat Mapilli Barat");
+    System.out.print("Kelas D");
+    System.out.println("Informatika");
+      }
+}
+  
