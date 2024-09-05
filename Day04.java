2e63b64730764629d50c19e127cce57ef849a9b4
Maryamm05 <myam5040@gmail.com>
2024-09-05 11:35:16 +0800

    Class04

---
+++ Day04.java
@@ -0,0 +1,6 @@
+public class day004{
+  public static void main(String[] args) {
+    System.out.printf("halo semuanya perkenalan:");
+    System.out.printf("nama saya %s dan saya berumur %d tahun.", "Maryam",19);
+  }
+}
