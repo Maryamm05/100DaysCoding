213421e4df0acfe90102feda342bd5b66f393c8f
Maryamm05 <myam5040@gmail.com>
2024-09-05 12:11:29 +0800

    Class4

--- Day04.java
+++ Day04.java
@@ -1,4 +1,4 @@
-public class day004{
+public class day04 {
   public static void main(String[] args) {
     System.out.println("Kon'nichiwa minasan:");
     System.out.printf("watashi no namae wa %s dan watashi no nenrei %d Nen.","Maryam",18);
