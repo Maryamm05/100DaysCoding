870121a614665d49d4ccde6660d88eef22e2bdcf
Maryamm05 <myam5040@gmail.com>
2024-09-06 11:44:34 +0800

    Class5

---
+++ Day05.java
@@ -0,0 +1,22 @@
+package Maryam;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Nama :");
+        System.out.println("Maryam");
+        System.out.print("Kelas :");
+        System.out.println("D informatika");
+        System.out.printf("Alamat saya %s dan saya berumur %d tahun", "Mapilli Barat",18);
+        
+        //Print berfungsi untuk menampilkan teks disatu baris tanpa pindah ke baris baru
+        //Println berfungsi untuk menampilkan teks dan langsung berpindah ke baris baru
+        
+        /*Sedangkan Printf beerfungsi untuk menampilkan teks dengan format tertentu, yang
+        sering digunakan untuk mencetak nilai dengan format yang telah ditentukan seperti
+        angka desimal, string ataupun karakter
+        */
+                
+    }
+    
+}
