9e9e42580809fbf7fa6b586fc22402149cd7d94c
Maryamm05 <myam5040@gmail.com>
2024-09-07 08:36:18 +0800

    Class6

---
+++ Day06.java
@@ -0,0 +1,14 @@
+package maryam_deklarasi_variabel;
+
+public class main {
+    public static void main(String[] args){
+    String nama;
+    int umur;
+    char gender;
+    
+    //String digunakan untuk teks
+    //int digunakan untuk bilangan bulat
+    //char untuk karakter 
+        
+    }
+}   
