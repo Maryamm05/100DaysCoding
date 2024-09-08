3f115ddb4fb9a0c3f651067cf23628b56fc547b3
Maryamm05 <myam5040@gmail.com>
2024-09-08 09:07:30 +0800

    Class7

---
+++ Day07.java
@@ -0,0 +1,14 @@
+package maryam_variabel_inisialisasi;
+public class Maryam_Variabel_Inisialisasi {
+    
+    public static void main(String[] args) {
+        String nama = "Maryam";
+        int umur = 18;
+        char gender = 'P';
+        float tinggi = (float) 153.5;
+        Boolean manis = true;
+        
+
+    }
+    
+} 
