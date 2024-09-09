692bb6c24a05bed4d032e3622af6436baea44f4b
Maryamm05 <myam5040@gmail.com>
2024-09-09 14:33:32 +0800

    class8

---
+++ Day08.java
@@ -0,0 +1,20 @@
+package coding;
+
+public class Main {
+    public static void main(String[] args) {
+        //Sebelum di update
+String nama = "Iyang";
+System.out.println ("Nama =" + nama);
+
+//Setelah di update
+nama = "Maryam";
+System.out.println ("Nama =" + nama);
+
+int umur = 18;
+System.out.println ("umur =" + umur);
+
+umur = 19;
+System.out.println ("umur =" + umur);
+    }
+    
+}
