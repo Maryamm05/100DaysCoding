7ad8bee13623570fb8997238f472f3aa23130779
Maryamm05 <myam5040@gmail.com>
2024-09-12 01:18:01 +0800

    Class11

---
+++ Day11.java
@@ -0,0 +1,14 @@
+public class main{
+public static void main (String[]args) {
+/*short adalah tipe data dalam pemrograman yang digunakan untuk menyimpan bilangan bulat:
+Nilai minimumnya adalah -32.768 
+Dan nilai maksimumnya adalah 32.767.
+ short */
+
+    short minimal= -32768;
+    short maksimal = 32767;
+    System.out.println("minimal = " + minimal );
+    System.out.println("maksimal = " + maksimal);
+    }
+    
+}
