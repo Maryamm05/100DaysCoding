fa6a4ce2b12bc583d214ab886f5a8fc969e1b5e2
Maryamm05 <myam5040@gmail.com>
2024-09-11 09:05:55 +0800

    Class10

---
+++ Day10.java
@@ -0,0 +1,9 @@
+package day10.java;
+public class Day10Java{
+
+    public static void main(String[] args) {
+        byte banyak = 15;
+        System.out.println("Banyaknya buku = " + banyak );
+        
+    }
+}
