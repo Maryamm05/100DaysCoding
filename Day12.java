21f6bbb289446e6fe823a398d8ba2f21ca537f0f
Maryamm05 <myam5040@gmail.com>
2024-09-13 06:34:35 +0800

    Class12

---
+++ Day12.java
@@ -0,0 +1,12 @@
+public class main{
+public static void main (String[]args) {
+
+/*Tipe data int (integer) adalah tipe data yang digunakan untuk 
+ merepresentasikan bilangan bulat, baik positif, negatif, maupun nol,
+ tanpa ada bagian desimal
+ */
+    int jumlah = 15;
+    System.out.println("Jumlah kucing = " + jumlah);
+    }
+    
+}
