19a875b5dd21c24c70c223cf6866b49eab71131d
Maryamm05 <myam5040@gmail.com>
2024-09-15 07:20:08 +0800

    Class14

---
+++ Day14.java
@@ -0,0 +1,14 @@
+package tipe.data.pkglong;
+
+public class TipeDataLong {
+
+    public static void main(String[] args) {
+    /*Tipe data  long merupakan tipe data yang digunakan untuk menyimpan 
+        bilangan bulat, fungsinya untuk menyimpan bilangan bulat yang 
+        kapasitasnya lebih besar dari type data byte, short dan integer.
+        */
+      
+        long tinggi = 154;
+        System.out.println("Tinggi badan = " + tinggi);
+    }
+}
