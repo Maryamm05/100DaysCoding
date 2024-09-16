95c333b66a29f22378c1e48f673d2943f33bf6cc
Maryamm05 <myam5040@gmail.com>
2024-09-16 11:37:14 +0800

    Class15

---
+++ Day15.java
@@ -0,0 +1,14 @@
+package day015;
+public class Day015 {
+
+    public static void main(String[] args) {
+    /*Tipe data double meimiliki ukuran 8 byte (64 bit),tipe data ini
+        merupakan tipe data numerik dalam pemrograman yang digunakan
+        untuk penyimpan bilangan desimal dengan kapasitas yang lebih 
+        tinggi dari tipe data float*/
+    
+        double berat = 43.25;
+        System.out.println("Berat Badan = " + berat + " kg ");
+    }
+    
+} 
