27ac334ea9d3c248af148fb6c8cfa2c78b0a5865
Maryamm05 <myam5040@gmail.com>
2024-09-18 07:49:08 +0800

    Class17

---
+++ Day17.java
@@ -0,0 +1,13 @@
+package day.pkg17;
+
+public class Day17 {
+
+    public static void main(String[] args) {
+    /*char merupakan tipe data primitif yang hanya bisa
+        menampung satu karakter saja*/
+         
+    char kelas = 'D';
+    System.out.println("Kelas Informatika = " + kelas);
+    }
+    
+}
