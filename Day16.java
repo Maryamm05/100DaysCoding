ae631099a625cccc8acccf0a12f403ae2a6e5493
Maryamm05 <myam5040@gmail.com>
2024-09-17 14:08:52 +0800

    Class16

---
+++ Day16.java
@@ -0,0 +1,14 @@
+package Tipe Data float;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        /*Tipe data float adalah tipe data yang digunakan
+         untuk menyimpan angka desimal atau bilangan dengan titik pecahan.
+         */
+        float panjang =14.5f;
+        System.out.println("Panjang papan = " + panjang + "cm");
+
+    }
+    
+}
