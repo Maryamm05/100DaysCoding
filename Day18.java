55172174ba0326a23e1900ecf8bcb85cad4b24a1
Maryamm05 <myam5040@gmail.com>
2024-09-19 10:44:25 +0800

    Class18

---
+++ Day18.java
@@ -0,0 +1,13 @@
+public class TipeDataBoolean {
+    
+    public static void main(String[] args) {
+    /*Tipe data boolean merupakan tipe data primitif yang hanya memiliki
+        dua nilai yaitu true(benar) dan false (salah). Salah satu fungsi dari 
+        boolean adalah sebagai penanda untuk kondisi tertentu dalam suatu
+        pemrograman*/
+    
+    boolean manis = true;
+    System.out.println("Apakah saya terlihat sangat manis : " + manis);
+        
+    }
+}
