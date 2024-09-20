0761751fd68d49afffec220400e9d875334b49c5
Maryamm05 <myam5040@gmail.com>
2024-09-20 17:20:29 +0800

    Class19

--- Day19.java
+++ Day19.java
@@ -3,9 +3,12 @@ package tipe.data.string;
 public class TipeDataString {
 
     public static void main(String[] args) {
-       String nama = "Maryam";
-       String alamat = "Mapilli barat";
+    /*tipe data string merupakan tipe data non primitif yang digunakan untuk 
+    merepresentasikan dan menyimpan teks yaitu serangkaian karakter*/
+    
+       String namaSaya = "Maryam";
+       System.out.println(namaSaya);
        
     }
     
-} 
+}
