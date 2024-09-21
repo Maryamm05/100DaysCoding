b1beabc86c6d0bd564971ee34ee8e00a6675cc9b
Maryamm05 <myam5040@gmail.com>
2024-09-21 21:05:55 +0800

    Class20

---
+++ Day20.java
@@ -0,0 +1,11 @@
+public class TipeDataByte {
+    public static void main(String[] args) {
+/*Tipe data reference byte mrupakan tipe data nonprimitif,letak
+perbedaannya itu berada diawal penulisan, tipe data non primitif awalannya huruf 
+kapital sedangkan tipe data primitif menggunakan huruf kecil di awalan penulisannya */
+
+    Byte banyak = 12;
+        System.out.println("Banyaknya buku = " + banyak );
+        
+    }
+} 
