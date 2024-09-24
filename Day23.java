edf8ebde9ca3fe39b62d16c0e20583d36452b53b
Maryamm05 <myam5040@gmail.com>
2024-09-24 21:10:09 +0800

    Class23

--- Day23.java
+++ Day23.java
@@ -3,7 +3,7 @@ public class TipeDataLong {
     public static void main(String[] args) {  
 //Contoh dari tipe data reference Long
 
-    Long nomor  = 1000;
+    Long nomor  = 1000L;
    System.out.println(nomor);
        
     }
