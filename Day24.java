62d9c27da0a50fad418592b9708379e1336918a0
Maryamm05 <myam5040@gmail.com>
2024-09-25 18:03:54 +0800

    Class24

---
+++ Day24.java
@@ -0,0 +1,10 @@
+public class day24 {
+    public static void main(String[] args) {
+    //contoh tipe data reference float
+        
+    Float berat = 43.5f;
+    System.out.println("Berat badan = " + berat);
+    
+    }
+    
+}
