32446060cc459754fc11c57cddeb01d97f3c5ef8
Maryamm05 <myam5040@gmail.com>
2024-09-27 01:17:00 +0800

    Class26

---
+++ Day26.java
@@ -0,0 +1,10 @@
+public class day26 {
+    public static void main(String[] args) {
+    //contoh tipe data reference Character 
+        
+    Character huruf = 'D';
+    System.out.println(" Informatika kelas = " + D );
+    
+    }
+    
+}
