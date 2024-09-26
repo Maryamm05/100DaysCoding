c395e91fa0b43a1b26084ccafd878901297bbfce
Maryamm05 <myam5040@gmail.com>
2024-09-26 09:13:29 +0800

    Class25

---
+++ Day25.java
@@ -0,0 +1,10 @@
+public class day25 {
+    public static void main(String[] args) {
+    //contoh tipe data reference Double
+        
+    Double tinggi = 153.5;
+    System.out.println("Tinggi badan = " + tinggi);
+    
+    }
+    
+}
