b0f6fac1c6d3756bd439cab5d7131da5176279a3
Maryamm05 <myam5040@gmail.com>
2024-09-28 09:52:17 +0800

    Class27

---
+++ Day27.java
@@ -0,0 +1,10 @@
+public class evaluasi {
+public static void main (String... args) {
+//Evaluasi besar-besaranan
+
+//System.out.println("Hello world");
+String iyang = "Hellow world";
+System.out.println(iyang);
+
+   }
+}
