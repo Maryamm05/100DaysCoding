42fd3c80556a08885a2bee5a47d8b705f5dbcdc3
Maryamm05 <myam5040@gmail.com>
2024-09-29 19:19:43 +0800

    Class28

---
+++ Day28.java
@@ -0,0 +1,10 @@
+public class day28 {
+    public static void main(String[] args) {
+    //contoh tipe data reference Boolean
+        
+    Boolean manis = true;
+    System.out.println(" Saya manis = " + manis );
+    
+    }
+    
+}
