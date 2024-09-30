007277bb2d651d85ce161934f3abf43b3825c22b
Maryamm05 <myam5040@gmail.com>
2024-09-30 22:09:15 +0800

    Class29

---
+++ Day29.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+public class bigInteger {
+    public static void main(String[] args) {
+    //contoh tipe data reference BigInteger
+      
+    BigInteger tes = new BigInteger("5000");
+    System.out.println("Harga barang = " + tes);
+   
+    }
+         
+}
