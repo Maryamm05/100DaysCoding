1458637f25b4eb2f33cc0ccaabb88b5c839c8c49
Maryamm05 <myam5040@gmail.com>
2024-10-01 20:07:12 +0800

    Class30

---
+++ Day30.java
@@ -0,0 +1,14 @@
+package day029;
+
+import java.math.BigDecimal;
+
+public class tipeDataBigDecimal {
+    public static void main(String[] args) {
+        
+        // contoh tipe data reference BigDecimal
+        
+        BigDecimal tos = new BigDecimal("153.5");
+        System.out.println(" Tinggi badan = " + tos);
+    }
+
+}
