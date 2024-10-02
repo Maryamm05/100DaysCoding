03354ec9369d8b9e1806a5e8aa33bad08bdf8dcc
Maryamm05 <myam5040@gmail.com>
2024-10-02 23:07:02 +0800

    Class31

---
+++ Day31.java
@@ -0,0 +1,10 @@
+public class day31 {
+    public static void main(String[] args) {
+        //contoh konversi String ke tipe data primitif
+        
+    String ang = "100";
+    int angka = Integer.parseInt(ang);
+        System.out.println(angka);
+    }
+  
+}
