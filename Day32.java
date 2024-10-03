d6b7698e95016bfbf75d223fe3abb02e454ff8f2
Maryamm05 <myam5040@gmail.com>
2024-10-03 19:20:02 +0800

    Class32

---
+++ Day32.java
@@ -0,0 +1,14 @@
+public class day32 {
+    public static void main(String[] args) {
+        /*contoh konversi data primitif ke String, yang dimana fungsinya itu untuk
+        mengubah data menjadi teks, sehingga bisa ditampilkan atau digabungkan
+        dengan teks yang lainnya.
+        */
+        
+        int Gojo = 190;
+        String TinggiGojo = Integer.toString(Gojo);
+        System.out.println(TinggiGojo);
+        
+    }
+  
+}
