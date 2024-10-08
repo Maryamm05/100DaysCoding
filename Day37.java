ae41f04118d354475e75d9322a707e0b7be13759
Maryamm05 <myam5040@gmail.com>
2024-10-08 22:07:11 +0800

    Class37

---
+++ Day37.java
@@ -0,0 +1,15 @@
+public class day37 {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 12; // 10=12 = 22
+        
+        int b = 30;
+        b-= 15; //30-15=15
+        
+        System.out.println("Nilai a : " + a);
+        System.out.println("Nilai b : " + b);
+        
+        
+    }
+    
+} 
