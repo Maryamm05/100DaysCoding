3fab3f84e747e6536b2c77e80359a93200fb8a30
Maryamm05 <myam5040@gmail.com>
2024-10-09 00:45:46 +0800

    Class38

---
+++ Day38.java
@@ -0,0 +1,13 @@
+public class day38 {
+    public static void main(String[] args) {
+        int a = 2;
+        a *= 4; 
+        
+        int b = 10;
+        b/= 5;
+        
+        System.out.println("Nilai perkalian dari variabel a : " + a);
+        System.out.println("Nilai pembagian dari variabel b : " + b);
+        
+        
+    }
