35039cee66abc033649af4a1342a3ee99eb0ac53
Maryamm05 <myam5040@gmail.com>
2024-10-10 04:43:51 +0800

    Class39

---
+++ Day39.java
@@ -0,0 +1,13 @@
+public class day39 {
+    public static void main(String[] args) {
+        
+    int a =10;
+    a %=2;
+    
+    int b = 8;
+    b%=3;
+    
+        System.out.println("Masukan nilai a : " + a);
+        System.out.println("Masukan nilai b : " + b);
+    }
+} 
