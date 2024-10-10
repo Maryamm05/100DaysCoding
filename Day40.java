b05821cdaeb811692d63b26cbb6179ef5ea1f139
Maryamm05 <myam5040@gmail.com>
2024-10-11 00:43:55 +0800

    Class40

---
+++ Day40.java
@@ -0,0 +1,12 @@
+public class day40 {
+    public static void main(String[] args) {
+    /*operator perbandingan merupakan operator yang digunakan untuk 
+      membandingkan dua nilai atau ekspresi dalam suatu program... 
+    */
+        int a = 10;
+        int b = 5;
+        
+        System.out.println( a > b); 
+        System.out.println( a < b);
+    }
+}
