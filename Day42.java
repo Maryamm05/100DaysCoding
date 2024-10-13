d7b19fade67404294c879b1bec2d8969d85740c6
Maryamm05 <myam5040@gmail.com>
2024-10-13 10:26:50 +0800

    Class42

---
+++ Day42.java
@@ -0,0 +1,13 @@
+public class day42 {
+    public static void main(String[] args) {
+        //Operator perbandingan = dan !=
+    
+        int a = 5;
+        int b = 5;
+        
+        System.out.println( a ==b);
+        System.out.println( a !=b);
+    
+    }
+ 
+}
