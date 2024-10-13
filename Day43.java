d032709e13f4b0225417c54f72d6549030fa6638
Maryamm05 <myam5040@gmail.com>
2024-10-14 06:16:56 +0800

    Class43

---
+++ Day43.java
@@ -0,0 +1,13 @@
+public class day43 {
+    public static void main(String[] args) {
+        //Operator perbandingan <= dan >=
+    
+        int a = 5;
+        int b = 10;
+        
+        System.out.println( a <=b);
+        System.out.println( a >=b);
+    
+    }
+    
+}
