39c5a7bc17c3606450cf720544111b63d03814e3
Maryamm05 <myam5040@gmail.com>
2024-10-15 01:20:23 +0800

    Class44

---
+++ Day44.java
@@ -0,0 +1,19 @@
+public class day44 {
+    public static void main(String[] args) {
+        
+        int nilai1 = 10;
+        int nilai2 = 20;
+        
+        //Menggunakan operator AND (&&)
+        
+        boolean hasil1 = (nilai1 < 15 && nilai2 > 15 );
+        boolean hasil2 = (nilai1 > 15 && nilai2 > 15 );
+        
+        System.out.println(hasil1);
+        System.out.println(hasil2);
+        
+        
+        
+    }
+  
+}
