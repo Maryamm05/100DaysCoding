7ca0aeb80b4b7d50adc6f8b6513983943f5da71e
Maryamm05 <myam5040@gmail.com>
2024-10-16 00:05:37 +0800

    Class45

---
+++ Day45.java
@@ -0,0 +1,19 @@
+public class day45 {
+    public static void main(String[] args) {
+        
+        int nilai1 = 5;
+        int nilai2 = 10;
+        
+        //Menggunakan operator OR (||)
+        
+        boolean hasil1 = (nilai1 < 15 || nilai2 > 15 );
+        boolean hasil2 = (nilai1 > 15 || nilai2 < 15 );
+        
+        System.out.println(hasil1);
+        System.out.println(hasil2);
+        
+        
+        
+    }
+  
+}
