1811d00a12b30fe9501e398ef2042cb4a48ae1b7
Maryamm05 <myam5040@gmail.com>
2024-10-17 00:03:40 +0800

    Class46

---
+++ Day46.java
@@ -0,0 +1,14 @@
+public class day46 {
+    public static void main(String[] args) {
+        
+        //Operator logika NOT (!)
+        
+        int nilai1 = 5;
+        int nilai2 = 10;
+        
+        boolean hasil = !(nilai1 < nilai2);
+        
+        System.out.println(hasil);
+    }
+  
+}
