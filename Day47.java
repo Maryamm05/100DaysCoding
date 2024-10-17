7fc79cf0b81e4127d66741426125f7093ffeeff8
Maryamm05 <myam5040@gmail.com>
2024-10-18 00:29:33 +0800

    Class47

--- Day47.java
+++ Day47.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class day47 {
     public static void main(String[] args) {
         Scanner tes = new Scanner(System.in);
-        
+       
         int a = tes.nextInt();
-        
-        if (a == 0){
-        
-        }else if (a % 2 == 0){
-            System.out.println("bilangan " + a + " adalah genap");
-        }else {
+      
+        if(a % 2 == 0 && a!= 0){
+           System.out.println("bilangan " + a + " adalah genap");
+        }if(a % 2 == 1){
             System.out.println("bilangan " + a + " adalah ganjil");
-            
         }
     }
 }
