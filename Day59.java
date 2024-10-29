2bc90837bef696973fa4a3bd21313f22fea9908a
Maryamm05 <myam5040@gmail.com>
2024-10-30 00:16:44 +0800

    Class59

---
+++ Day59.java
@@ -0,0 +1,9 @@
+public class day59 {
+    public static void main(String[] args) {
+        String [] abjad = {"M","A","R","Y","A","M"};
+        
+        for (String i : abjad){
+            System.out.print(i + " "); 
+        }
+    }
+}
