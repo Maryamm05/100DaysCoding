c2e91879cfabc1b6075101da58cb6940c2710988
Maryamm05 <myam5040@gmail.com>
2024-11-08 23:39:21 +0800

    Class68

---
+++ Day68.java
@@ -0,0 +1,9 @@
+public class NonVoid {
+    public static void main(String[] args) {
+       NonVoid yamm = new NonVoid();
+        System.out.println(yamm.angka());
+    }
+     int angka(){
+        return 19;
+     }
+}
