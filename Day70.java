00702aeb3f6dcf24553d358e59489ee8dace8160
Maryamm05 <myam5040@gmail.com>
2024-11-10 20:18:33 +0800

    Class70

---
+++ Day70.java
@@ -0,0 +1,13 @@
+public class Main {
+    
+    public static int hitungKuadrat(int x) {
+        return x * x;
+    }
+    public static void main(String[] args) {
+        int angka = 5;
+
+        int hasil = hitungKuadrat(angka);
+      
+        System.out.println(hasil);
+    }
+}
