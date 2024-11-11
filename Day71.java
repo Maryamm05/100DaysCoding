acc6c14f4d5bcabbc230572b1830af1d730e0b5e
Maryamm05 <myam5040@gmail.com>
2024-11-11 23:20:46 +0800

    Class71

---
+++ Day71.java
@@ -0,0 +1,11 @@
+public class day71 {
+    
+    public static void Hayy() {
+        System.out.println("Hallo, Guys!");
+    }
+    
+    public static void main(String[] args) {
+    
+        Hayy();
+    }
+}
