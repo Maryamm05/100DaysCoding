b571ef012a65c9375a22eaff9095f278e590f902
Maryamm05 <myam5040@gmail.com>
2024-11-12 21:02:26 +0800

    Class72

---
+++ Day72.java
@@ -0,0 +1,15 @@
+public class PenjumlahanRekursif {
+
+    public static int hitungPenjumlahan(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + hitungPenjumlahan(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int angka = 5;
+        int hasil = hitungPenjumlahan(angka);
+        System.out.println("hasil);
+    }
+}
