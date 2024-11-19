34677859ec8ea03870c7111e57062e42f8fd92d6
Maryamm05 <myam5040@gmail.com>
2024-11-19 21:12:57 +0800

    Clasa79

---
+++ Day79.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Day79 {
+
+    public static void main(String[] args) {
+        
+        Scanner tes = new Scanner(System.in);
+        String nama = tes.nextLine();
+
+       System.out.println(nama.toString());
+    }
+}
