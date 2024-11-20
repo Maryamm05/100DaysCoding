30568e2cd10d060d24515a8e892b48320b079279
Maryamm05 <myam5040@gmail.com>
2024-11-20 22:02:00 +0800

    Class80

---
+++ Day80.java
@@ -0,0 +1,8 @@
+public class day80 {
+   public static void main(String[]args){
+       String a = "  Iyang  ";
+       
+       System.out.println("Hasil menggunakan spasi : '"+a+"'");
+           System.out.println("Hasil tanpa spasi : '" + a.trim() + "'");
+   } 
+}
