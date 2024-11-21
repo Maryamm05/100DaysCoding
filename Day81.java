9c526ea52074cb8ad09e4e32395def2aaf879acd
Maryamm05 <myam5040@gmail.com>
2024-11-21 21:14:41 +0800

    Clasa81

---
+++ Day81.java
@@ -0,0 +1,10 @@
+public class day81 {
+    public static void main(String[] args) {
+
+        int ang = 40;
+        String angka = String.valueOf(ang);
+      
+        System.out.println(angka);
+       
+    }  
+}
