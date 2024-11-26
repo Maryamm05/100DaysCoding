302b580bf2af22c5a82e7e2705a4d054099406b5
Maryamm05 <myam5040@gmail.com>
2024-11-26 20:00:26 +0800

    Class86

---
+++ Day86.java
@@ -0,0 +1,12 @@
+public class day86 {
+    public static void main(String...args){
+        int [] angka = {10,15,20,25,30};
+        
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+    }
+
+
+    
+}
