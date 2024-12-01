04af9299c429fb5bec153e3ed6badbcf6e0193f0
Maryamm05 <myam5040@gmail.com>
2024-12-01 21:55:51 +0800

    Class91

---
+++ Day91.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class day91 {
+    public static void main(String[] args) {
+        Scanner tes = new Scanner(System.in);
+        
+        int nilai = tes.nextInt();
+        
+        if(nilai >= 0 ){
+            System.out.println("kita baikan");
+        }else {
+            System.out.println("you and i, end");
+        }
+    }
+ 
+}
