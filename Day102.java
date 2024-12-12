b6001c1761a8cdd12c98e6956113391562adfa1b
Maryamm05 <myam5040@gmail.com>
2024-12-12 22:49:39 +0800

    Clasa102

---
+++ Day102.java
@@ -0,0 +1,8 @@
+public class day102 {
+   public static void main(String...args){
+       String nama = "Iyang";
+       String gula = " Manisnya kebanyakan";
+       
+       System.out.println(nama + gula);
+   }
+}
